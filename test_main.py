87550498cb251f38a71a2197ead3eb92cb82a043
moonyjr <125967473+moonyjr@users.noreply.github.com>
2025-10-22 00:01:03 +0800

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,12 @@
+# test_main.py
+import pytest
+from main import add, subtract
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_subtract():
+    assert subtract(10, 5) == 5
+
+if __name__ == '__main__':
+    pytest.main()
