5be3b776c4b0696395ac0f0591f7f58d55a6bff7
moonyjr <125967473+moonyjr@users.noreply.github.com>
2025-10-21 23:59:20 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == '__main__':
+    result1 = add(10, 5)
+    result2 = subtract(10, 5)
+    print("Addition:", result1)
+    print("Subtraction:", result2)
